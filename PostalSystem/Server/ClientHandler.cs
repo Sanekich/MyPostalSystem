20c1ea9bb8c7d152f419a13f130e208b57ad9887
Oleksandr Muratov <153177556+Sanekich@users.noreply.github.com>
2024-06-22 11:54:26 +0300

    Add files via upload

---
+++ PostalSystem/Server/ClientHandler.cs
@@ -0,0 +1,259 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Models;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace Server
+{
+    public class ClientHandler
+    {
+        private readonly UdpClient udpServer;
+        private readonly IConfigurationRoot config;
+        private Server server;
+
+        public ClientHandler(UdpClient server, IConfigurationRoot config, Server serv)
+        {
+            udpServer = server;
+            this.config = config;
+            this.server = serv;
+        }
+
+        public async Task HandleClientAsync(CancellationToken token)
+        {
+            try
+            {
+                while (true)
+                {
+                    token.ThrowIfCancellationRequested();
+                    UdpReceiveResult res = await udpServer.ReceiveAsync(token);
+                    byte[] buf = res.Buffer;
+                    string message = Encoding.UTF8.GetString(buf);
+                    using SqlConnection connection = new(config.GetConnectionString("SqlClient"));
+                    switch (message[0])
+                    {
+                        case 'L':
+                            {
+                                string[] loginParams = message.Split(';');
+                                if (loginParams.Length == 3)
+                                {
+                                    string login = loginParams[1];
+                                    string password = loginParams[2];
+                                    int count = await CountClientsWithLoginAndPassword(connection, login, password);
+                                    byte[] loginResponseBytes;
+                                    if (count == 1)
+                                    {
+                                        loginResponseBytes = Encoding.UTF8.GetBytes("User exists");
+                                        AddToLog($"User {login} logged in");
+                                    }
+                                    else
+                                    {
+                                        loginResponseBytes = Encoding.UTF8.GetBytes("User does not exist");
+                                    }
+                                    await udpServer.SendAsync(loginResponseBytes, loginResponseBytes.Length, res.RemoteEndPoint);
+                                }
+                                else
+                                {
+                                    string invalidFormatMessage = "Invalid login request format";
+                                    byte[] invalidFormatBytes = Encoding.UTF8.GetBytes(invalidFormatMessage);
+                                    await udpServer.SendAsync(invalidFormatBytes, invalidFormatBytes.Length, res.RemoteEndPoint);
+                                }
+                            }
+                            break;
+                        case 'R':
+                            {
+                                string[] registerParams = message.Split(';');
+                                if (registerParams.Length == 3)
+                                {
+                                    string login = registerParams[1];
+                                    string password = registerParams[2];
+                                    int count = await CountClientsWithLoginAndPassword(connection, login, password);
+                                    if (count == 0)
+                                    {
+                                        await RegisterNewUserAsync(connection, login, password);
+                                        AddToLog($"User {login} registered.");
+                                        string registrationSuccessMessage = "Registration successful";
+                                        byte[] registrationSuccessBytes = Encoding.UTF8.GetBytes(registrationSuccessMessage);
+                                        await udpServer.SendAsync(registrationSuccessBytes, registrationSuccessBytes.Length, res.RemoteEndPoint);
+                                    }
+                                    else
+                                    {
+                                        string userExistsMessage = "User with this login already exists";
+                                        byte[] userExistsBytes = Encoding.UTF8.GetBytes(userExistsMessage);
+                                        await udpServer.SendAsync(userExistsBytes, userExistsBytes.Length, res.RemoteEndPoint);
+                                    }
+                                }
+                                else
+                                {
+                                    string invalidFormatMessage = "Invalid registration request format";
+                                    byte[] invalidFormatBytes = Encoding.UTF8.GetBytes(invalidFormatMessage);
+                                    await udpServer.SendAsync(invalidFormatBytes, invalidFormatBytes.Length, res.RemoteEndPoint);
+                                }
+                            }
+                            break;
+                        case 'B':
+                            {
+                                IEnumerable<dynamic> data = await connection.QueryAsync("select Id, X, Y from Buildings");
+                                List<string> buildings = [];
+                                int count = 1;
+                                foreach (dynamic b in data)
+                                {
+                                    buildings.Add($"Post office number {count}   ({b.X}, {b.Y})");
+                                    count++;
+                                }
+                                buf = JsonSerializer.SerializeToUtf8Bytes(buildings);
+                                await udpServer.SendAsync(buf, buf.Length, res.RemoteEndPoint);
+                            }
+                            break;
+                        case 'T':
+                            {
+                                IEnumerable<string> data = await connection.QueryAsync<string>("select Type from ParcelTypes");
+                                List<string> types = [];
+                                types.AddRange(data);
+                                buf = JsonSerializer.SerializeToUtf8Bytes(types);
+                                await udpServer.SendAsync(buf, buf.Length, res.RemoteEndPoint);
+                            }
+                            break;
+                        case 'P':
+                            {
+                                string login = message.Split(";")[1];
+                                IEnumerable<ParcelModel> data = await connection.QueryAsync<ParcelModel>("select * from Parcels as p join Clients as c on (p.SenderId = c.Id or p.ReceiverId = c.Id) and c.Login = @clientLogin", new { clientLogin = login });
+                                buf = JsonSerializer.SerializeToUtf8Bytes(data);                               
+                                await udpServer.SendAsync(buf, buf.Length, res.RemoteEndPoint);
+                            }
+                            break;
+                        case 'C':
+                            {
+                                string[] parcelParams = message.Split(';');
+                                if (parcelParams.Length == 7)
+                                {
+                                    try
+                                    {
+                                        string clientLogin = parcelParams[1];
+                                        string receiverLogin = parcelParams[2];
+                                        string type = parcelParams[3];
+                                        string description = parcelParams[4];
+                                        int fromBuildingId = int.Parse(parcelParams[5]);
+                                        int toBuildingId = int.Parse(parcelParams[6]);
+
+
+
+                                        int senderId = await GetClientIdByLogin(connection, clientLogin);
+                                        int receiverId = await GetClientIdByLogin(connection, receiverLogin);
+
+                                        if (senderId != 0 && receiverId != 0)
+                                        {
+                                            await InsertParcelAsync(connection, senderId, receiverId, fromBuildingId, toBuildingId, type, description);
+
+                                            string successMessage = "Parcel created successfully";
+                                            byte[] successBytes = Encoding.UTF8.GetBytes(successMessage);
+                                            await udpServer.SendAsync(successBytes, successBytes.Length, res.RemoteEndPoint);
+                                        }
+                                        else
+                                        {
+                                            string errorMessage = "Sender or receiver not found";
+                                            byte[] errorBytes = Encoding.UTF8.GetBytes(errorMessage);
+                                            await udpServer.SendAsync(errorBytes, errorBytes.Length, res.RemoteEndPoint);
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        string errorMessage = "Error creating parcel: " + ex.Message;
+                                        byte[] errorBytes = Encoding.UTF8.GetBytes(errorMessage);
+                                        await udpServer.SendAsync(errorBytes, errorBytes.Length, res.RemoteEndPoint);
+                                    }
+                                }
+                                else
+                                {
+                                    string invalidFormatMessage = "Invalid parcel creation request format";
+                                    byte[] invalidFormatBytes = Encoding.UTF8.GetBytes(invalidFormatMessage);
+                                    await udpServer.SendAsync(invalidFormatBytes, invalidFormatBytes.Length, res.RemoteEndPoint);
+                                }                                
+                            }
+                            break;
+                            default:
+                            {
+                                ParcelModel parcel = JsonSerializer.Deserialize<ParcelModel>(message)!;
+                                string data = "";
+                                data += "Sender - " + await connection.ExecuteScalarAsync<string>("select Login from Clients where Id = @id", new { id = parcel.SenderId }) + "\n";
+                                data += "Receiver - " + await connection.ExecuteScalarAsync<string>("select Login from Clients where Id = @id", new { id = parcel.ReceiverId }) + "\n";
+                                data += "From post office number " + await connection.ExecuteScalarAsync<string>("select Id from Buildings where Id = @id", new { id = parcel.FromBuildingId }) + "\n";
+                                data += "To post office number " + await connection.ExecuteScalarAsync<string>("select Id from Buildings where Id = @id", new { id = parcel.ToBuildingId }) + "\n";
+                                data += "Type - " + await connection.ExecuteScalarAsync<string>("select Type from ParcelTypes where Id = @id", new { id = parcel.TypeId }) + "\n";
+                                data += "Distance - " + parcel.Distance + "\n";
+                                data += "Cost - " + parcel.Cost + "\n";
+                                byte[] tmp = Encoding.UTF8.GetBytes(data);
+                                await udpServer.SendAsync(tmp, tmp.Length, res.RemoteEndPoint);
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("The operation was cancelled", "Cancelled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        async Task<int> CountClientsWithLoginAndPassword(SqlConnection connection, string login, string password)
+        {
+            string sql = "SELECT COUNT(*) FROM Clients WHERE Login = @Login AND Password = @Password";
+            return await connection.ExecuteScalarAsync<int>(sql, new { Login = login, Password = password });
+        }
+
+        async Task RegisterNewUserAsync(SqlConnection connection, string login, string password)
+        {
+            string sql = "INSERT INTO Clients (Login, Password) VALUES (@Login, @Password)";
+            await connection.ExecuteAsync(sql, new { Login = login, Password = password });
+        }
+
+        async Task<int> GetClientIdByLogin(SqlConnection connection, string login)
+        {
+            string sql = "SELECT Id FROM Clients WHERE Login = @Login";
+            return await connection.ExecuteScalarAsync<int>(sql, new { Login = login });
+        }
+
+        async Task InsertParcelAsync(SqlConnection connection, int senderId, int receiverId, int fromBuildingId, int toBuildingId, string type, string description)
+        {
+            try
+            {
+                int typeId = await GetParcelTypeId(connection, type);
+
+                string sql = @"INSERT INTO Parcels (SenderId, ReceiverId, FromBuildingId, ToBuildingId, TypeId, Description) VALUES (@SenderId, @ReceiverId, @FromBuildingId, @ToBuildingId, @TypeId, @Description)";
+
+                await connection.ExecuteAsync(sql, new
+                {
+                    SenderId = senderId,
+                    ReceiverId = receiverId,
+                    FromBuildingId = fromBuildingId,
+                    ToBuildingId = toBuildingId,
+                    TypeId = typeId,
+                    Description = description
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting parcel: {ex.Message}");
+                throw;
+            }
+        }
+
+        async Task<int> GetParcelTypeId(SqlConnection connection, string type)
+        {
+            string sql = "SELECT Id FROM ParcelTypes WHERE Type = @Type";
+            return await connection.ExecuteScalarAsync<int>(sql, new { Type = type });
+        }
+
+        public void AddToLog(string msg)
+        {
+            server.AppendToLogs(msg);
+        }
+    }
+}
---
+++ PostalSystem/Server/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbLog = new ListBox();
+            bStart = new Button();
+            bStop = new Button();
+            nudX = new NumericUpDown();
+            nudY = new NumericUpDown();
+            bAddBuilding = new Button();
+            tbParcelType = new TextBox();
+            bAddParcelType = new Button();
+            nudCostMultiplier = new NumericUpDown();
+            bRemoveBuilding = new Button();
+            bRemoveParcelType = new Button();
+            cbSelect = new ComboBox();
+            bSelect = new Button();
+            ((System.ComponentModel.ISupportInitialize)nudX).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudY).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudCostMultiplier).BeginInit();
+            SuspendLayout();
+            // 
+            // lbLog
+            // 
+            lbLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lbLog.Enabled = false;
+            lbLog.FormattingEnabled = true;
+            lbLog.Location = new Point(292, 12);
+            lbLog.Name = "lbLog";
+            lbLog.Size = new Size(496, 424);
+            lbLog.TabIndex = 0;
+            // 
+            // bStart
+            // 
+            bStart.Location = new Point(12, 12);
+            bStart.Name = "bStart";
+            bStart.Size = new Size(137, 82);
+            bStart.TabIndex = 2;
+            bStart.Text = "Start server";
+            bStart.UseVisualStyleBackColor = true;
+            bStart.Click += bStart_Click;
+            // 
+            // bStop
+            // 
+            bStop.Enabled = false;
+            bStop.Location = new Point(149, 12);
+            bStop.Name = "bStop";
+            bStop.Size = new Size(137, 82);
+            bStop.TabIndex = 3;
+            bStop.Text = "Stop server";
+            bStop.UseVisualStyleBackColor = true;
+            bStop.Click += bStop_Click;
+            // 
+            // nudX
+            // 
+            nudX.Enabled = false;
+            nudX.Location = new Point(12, 100);
+            nudX.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
+            nudX.Name = "nudX";
+            nudX.Size = new Size(137, 27);
+            nudX.TabIndex = 4;
+            // 
+            // nudY
+            // 
+            nudY.Enabled = false;
+            nudY.Location = new Point(149, 100);
+            nudY.Maximum = new decimal(new int[] { 100000, 0, 0, 0 });
+            nudY.Name = "nudY";
+            nudY.Size = new Size(137, 27);
+            nudY.TabIndex = 5;
+            // 
+            // bAddBuilding
+            // 
+            bAddBuilding.Enabled = false;
+            bAddBuilding.Location = new Point(12, 133);
+            bAddBuilding.Name = "bAddBuilding";
+            bAddBuilding.Size = new Size(137, 82);
+            bAddBuilding.TabIndex = 6;
+            bAddBuilding.Text = "Add building";
+            bAddBuilding.UseVisualStyleBackColor = true;
+            bAddBuilding.Click += bAddBuilding_Click;
+            // 
+            // tbParcelType
+            // 
+            tbParcelType.Enabled = false;
+            tbParcelType.Location = new Point(12, 221);
+            tbParcelType.Name = "tbParcelType";
+            tbParcelType.PlaceholderText = "Parcel type";
+            tbParcelType.Size = new Size(207, 27);
+            tbParcelType.TabIndex = 7;
+            // 
+            // bAddParcelType
+            // 
+            bAddParcelType.Enabled = false;
+            bAddParcelType.Location = new Point(12, 254);
+            bAddParcelType.Name = "bAddParcelType";
+            bAddParcelType.Size = new Size(137, 82);
+            bAddParcelType.TabIndex = 8;
+            bAddParcelType.Text = "Add parcel type";
+            bAddParcelType.UseVisualStyleBackColor = true;
+            bAddParcelType.Click += bAddParcelType_Click;
+            // 
+            // nudCostMultiplier
+            // 
+            nudCostMultiplier.Enabled = false;
+            nudCostMultiplier.Location = new Point(225, 222);
+            nudCostMultiplier.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nudCostMultiplier.Name = "nudCostMultiplier";
+            nudCostMultiplier.Size = new Size(61, 27);
+            nudCostMultiplier.TabIndex = 9;
+            nudCostMultiplier.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // bRemoveBuilding
+            // 
+            bRemoveBuilding.Enabled = false;
+            bRemoveBuilding.Location = new Point(149, 133);
+            bRemoveBuilding.Name = "bRemoveBuilding";
+            bRemoveBuilding.Size = new Size(137, 82);
+            bRemoveBuilding.TabIndex = 10;
+            bRemoveBuilding.Text = "Remove building";
+            bRemoveBuilding.UseVisualStyleBackColor = true;
+            bRemoveBuilding.Click += bRemoveBuilding_Click;
+            // 
+            // bRemoveParcelType
+            // 
+            bRemoveParcelType.Enabled = false;
+            bRemoveParcelType.Location = new Point(149, 254);
+            bRemoveParcelType.Name = "bRemoveParcelType";
+            bRemoveParcelType.Size = new Size(137, 82);
+            bRemoveParcelType.TabIndex = 11;
+            bRemoveParcelType.Text = "Remove parcel type";
+            bRemoveParcelType.UseVisualStyleBackColor = true;
+            bRemoveParcelType.Click += bRemoveParcelType_Click;
+            // 
+            // cbSelect
+            // 
+            cbSelect.Enabled = false;
+            cbSelect.FormattingEnabled = true;
+            cbSelect.Location = new Point(12, 342);
+            cbSelect.Name = "cbSelect";
+            cbSelect.Size = new Size(274, 28);
+            cbSelect.TabIndex = 12;
+            cbSelect.Text = "Select table";
+            // 
+            // bSelect
+            // 
+            bSelect.Enabled = false;
+            bSelect.Location = new Point(12, 376);
+            bSelect.Name = "bSelect";
+            bSelect.Size = new Size(274, 60);
+            bSelect.TabIndex = 13;
+            bSelect.Text = "Select";
+            bSelect.UseVisualStyleBackColor = true;
+            bSelect.Click += bSelect_Click;
+            // 
+            // Server
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(bSelect);
+            Controls.Add(cbSelect);
+            Controls.Add(bRemoveParcelType);
+            Controls.Add(bRemoveBuilding);
+            Controls.Add(nudCostMultiplier);
+            Controls.Add(bAddParcelType);
+            Controls.Add(tbParcelType);
+            Controls.Add(bAddBuilding);
+            Controls.Add(nudY);
+            Controls.Add(nudX);
+            Controls.Add(bStop);
+            Controls.Add(bStart);
+            Controls.Add(lbLog);
+            Name = "Server";
+            Text = "Server";
+            ((System.ComponentModel.ISupportInitialize)nudX).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudY).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudCostMultiplier).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lbLog;
+        private Button bStart;
+        private Button bStop;
+        private NumericUpDown nudX;
+        private NumericUpDown nudY;
+        private Button bAddBuilding;
+        private TextBox tbParcelType;
+        private Button bAddParcelType;
+        private NumericUpDown nudCostMultiplier;
+        private Button bRemoveBuilding;
+        private Button bRemoveParcelType;
+        private ComboBox cbSelect;
+        private Button bSelect;
+    }
+}
