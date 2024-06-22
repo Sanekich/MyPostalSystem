0d25c661ce7ca8f6354df3fe699e04c562fc998a
Oleksandr Muratov <153177556+Sanekich@users.noreply.github.com>
2024-06-22 11:44:30 +0300

    Add files via upload

---
+++ PostalSystem/Models/ParcelModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Models
+{
+    public class ParcelModel
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public int FromBuildingId { get; set; }
+        public int ToBuildingId { get; set; }
+        public int TypeId { get; set; }
+        public string? Description { get; set; }
+        public int Distance { get; set; }
+        public int Cost { get; set; }
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
